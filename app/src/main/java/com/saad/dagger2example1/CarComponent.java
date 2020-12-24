aae5817b86170920d4bc9063d6ee1ac8d4bb87a6
Saad <atsaadhaqe@gmail.com>
2020-12-24 21:23:00 +0600

    Initial commit

---
+++ app/src/main/java/com/saad/dagger2example1/CarComponent.java
@@ -0,0 +1,10 @@
+package com.saad.dagger2example1;
+
+
+import dagger.Component;
+
+@Component
+public interface CarComponent {
+
+    Car getCar();
+}
---
+++ app/src/main/java/com/saad/dagger2example1/Wheels.java
@@ -0,0 +1,10 @@
+package com.saad.dagger2example1;
+
+import javax.inject.Inject;
+
+public class Wheels {
+
+    @Inject
+    public Wheels() {
+    }
+}
